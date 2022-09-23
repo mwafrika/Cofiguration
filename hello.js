6e8375abb58fd83beec0aab81678f47210d89eca
MWAFRIKA MUFUNGIZI <mwafrikajosue@gmail.com>
2022-09-23 11:56:58 +0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,1 @@
-Check if everything is fine
+React Query is often described as the missing data-fetching library for React, but in more technical terms, it makes fetching, caching, synchronizing and updating server state in your React applications a breeze.
